50efb992ac00c55c6ac8ba2ee7051d002cc8eb77
Kwadwo-Blanc <ebenezerghart@gmail.com>
2024-06-27 15:56:00 -0700

    spec 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,60 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._validateName(name);
+    this._validateLength(length);
+    this._validateStudents(students);
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  // Getter and setter for name
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._validateName(value);
+    this._name = value;
+  }
+
+  // Getter and setter for length
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    this._validateLength(value);
+    this._length = value;
+  }
+
+  // Getter and setter for students
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    this._validateStudents(value);
+    this._students = value;
+  }
+
+  // Validation methods
+  _validateName(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  _validateLength(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+  }
+
+  _validateStudents(value) {
+    if (!Array.isArray(value) || !value.every(student => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+  }
+}
